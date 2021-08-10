ee970fe51eaafab21a01a2a1d7bbf40767fac7ad
Gayathri32162 <gayathribalamurugan82@gmail.com>
2021-08-10 08:13:14 +0530

    made some changes

--- index.js
+++ index.js
@@ -2,26 +2,9 @@ const taskContainer = document.querySelector(".task__containers");
 let globalTaskData = [];
 
 
-const addNewCard = () => {
-    //get task data
-    const taskData = {
-        id: `${Date.now()}`,//template literal
-        title: document.getElementById("taskTitle").value,
-        Image:document.getElementById("imageURL").value,
-        type:document.getElementById("tasktype").value,
-        description:document.getElementById("taskdesc").value,
-    };
-
-
-    globalTaskData.push(taskData);
-
-//update local storage
-
-    localStorage.setItem("taskyCA" , JSON.stringify({ cards: globalTaskData}));
-
-
-//generate html code
-const newCard = ` <div id=${taskData.id} class="col-md-6 col-lg-4 my-4 ">
+const generateHTML = (
+  taskData
+) =>  ` <div id=${taskData.id} class="col-md-6 col-lg-4 my-4 ">
 <div class="card">
   <div class="card-header gap-2 d-flex justify-content-end">
     <button class="btn btn-outline-info ">
@@ -43,10 +26,32 @@ const newCard = ` <div id=${taskData.id} class="col-md-6 col-lg-4 my-4 ">
   </div>
 </div>
 </div> `;
+const insertToDOM = (content) =>
+  taskContainer.insertAdjacentHTML("beforeend", newCard);
 
 
+const addNewCard = () => {
+    //get task data
+    const taskData = {
+        id: `${Date.now()}`,//template literal
+        title: document.getElementById("taskTitle").value,
+        Image:document.getElementById("imageURL").value,
+        type:document.getElementById("tasktype").value,
+        description:document.getElementById("taskdesc").value,
+    };
 
-//inject it to dom
+
+    globalTaskData.push(taskData);
+
+//update local storage
+
+    localStorage.setItem("taskyCA" , JSON.stringify({ cards: globalTaskData}));
+
+
+//generate html code
+const newCard = generateHTML(taskData);
+
+insertToDOM(newCard);
 
 taskContainer.insertAdjacentHTML("beforeend", newCard);
 
@@ -78,34 +83,12 @@ const loadExistingCards = () => {
   globalTaskData.map((taskData) => {
 
    //generate html code for those data
+  const newCard = generateHTML(taskData);  
+  insertToDOM(newCard);
 
-  const newCard= ` <div id=${taskData.id} class="col-md-6 col-lg-4 my-4 ">
-<div class="card">
-  <div class="card-header gap-2 d-flex justify-content-end">
-    <button class="btn btn-outline-info ">
-      <i class="fad fa-pencil"></i>
-    </button>
-    <button class="btn btn-outline-danger">
-      <i class="fas fa-trash-alt"></i>
-    </button>
-  </div>
-  <div class="card-body">
-    <img src=${taskData.Image}
-    <h5 class="card-title mt-4">${taskData.title}</h5>
-    <p class="card-text">${taskData.description}</p>
-    <span class="badge bg-primary">${taskData.type}</span>
-    
-  </div>
-  <div class="card-footer ">
-    <button class="btn btn-outline-primary">open task</button>
-  </div>
-</div>
-</div> `;
-
-  
-  //inject to dom
-  taskContainer.insertAdjacentHTML("beforeend", newCard);
+  // taskContainer.insertAdjacentHTML("beforeend", newCard);
   });
+  return;
 
 
-}
+};
